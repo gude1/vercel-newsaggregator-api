43205b90aedd80a79110982e57b636eb46e84fcf
Owolabi Gideon <owolabigidslab@gmail.com>
2023-07-13 00:41:39 +0100

    vercel updates

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/t', function () {
-    return "hello";
-});
+Route::post("/signup", [UserController::class, "store"]);
